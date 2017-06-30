6f55e722c920defb815fe52e002159aadde5c626
wangheng <wangheng@mobimagic.com>
2017-06-30 16:51:07 +0800

    去掉无用的导入

--- library/src/main/java/com/droid/library/widget/FlowLayout.java
+++ library/src/main/java/com/droid/library/widget/FlowLayout.java
@@ -8,8 +8,6 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewGroup.LayoutParams;
-
 
 import com.xinye.lib.R;
 
